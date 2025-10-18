b8e1df8c9a87034e918657bf2426c44d68233f41
Anja Obradovic <163130723+anjaobradovic@users.noreply.github.com>
2025-10-18 11:22:22 +0200

    Init backend

---
+++ backend/src/main/java/rs/ftn/newnow/NewNowApplication.java
@@ -0,0 +1,13 @@
+package rs.ftn.newnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewNowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/rs/ftn/newnow/NewNowApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ftn.newnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
